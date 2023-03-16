d95e1052ed2dcf8867986e0217b3383b3d79df7b
leedawnn <2g_hee@naver.com>
2023-03-17 02:40:06 +0900

    feat: Modify Text

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>히히 짱이당 🤭</Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
