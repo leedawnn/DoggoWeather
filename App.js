eaabadb91afc67ba7f7ad854c27943904dc7a67b
leedawnn <2g_hee@naver.com>
2023-03-25 02:15:08 +0900

    feat: Setting layout, apply fonts

--- App.js
+++ App.js
@@ -1,9 +1,34 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useFonts } from '@expo-google-fonts/inter';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Ubuntu-light': require('./assets/fonts/Ubuntu-Light.ttf'),
+    'Ubuntu-regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
+    'Ubuntu-medium': require('./assets/fonts/Ubuntu-Medium.ttf'),
+    'Ubuntu-bold': require('./assets/fonts/Ubuntu-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>히히 짱이당 🤭</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.today}>Today</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
+      <View style={styles.DetailWeather}></View>
+      <StatusBar style='light' />
     </View>
   );
 }
@@ -11,12 +36,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'tomato',
   },
-  text: {
-    fontSize: 28,
+  city: {
+    flex: 1,
+    backgroundColor: 'royalblue',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
     color: '#ffffff',
+    fontFamily: 'Ubuntu-medium',
+    fontSize: 38,
+  },
+  weather: {
+    flex: 2,
+  },
+  day: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  today: {
+    marginTop: 50,
+    fontFamily: 'Ubuntu-medium',
+    fontSize: 36,
+  },
+  temp: {
+    marginTop: 10,
+    fontFamily: 'Ubuntu-bold',
+    fontSize: 92,
+  },
+  description: {
+    fontFamily: 'Ubuntu-regular',
+    fontSize: 24,
+  },
+  DetailWeather: {
+    flex: 2,
   },
 });
