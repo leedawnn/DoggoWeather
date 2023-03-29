593c1ab4d154a93e3c99f8afc08faf8075a14559
leedawnn <2g_hee@naver.com>
2023-03-30 00:33:04 +0900

    feat: Get location, display city name

--- App.js
+++ App.js
@@ -1,9 +1,38 @@
+import * as Location from 'expo-location';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useFonts } from '@expo-google-fonts/inter';
+import { useEffect, useState } from 'react';
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window');
 
 export default function App() {
+  const [city, setCity] = useState('Loading...');
+  const [location, setLocation] = useState();
+  const [ok, setOk] = useState(true);
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+
+    if (!granted) {
+      setOk(false);
+      setErrorMsg('위치 접근 권한이 거부되었습니다! 🥺');
+      return;
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync({ latitude, longitude }, { useGoogleMaps: false });
+
+    setCity(location[0].city);
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   const [fontsLoaded] = useFonts({
     'Ubuntu-light': require('./assets/fonts/Ubuntu-Light.ttf'),
     'Ubuntu-regular': require('./assets/fonts/Ubuntu-Regular.ttf'),
@@ -18,15 +47,40 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.city}>
-        <Text style={styles.cityName}>Seoul</Text>
+        <Text style={styles.cityName}>{city}</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
         <View style={styles.day}>
           <Text style={styles.today}>Today</Text>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.today}>Today</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.today}>Today</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.today}>Today</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.today}>Today</Text>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
       <View style={styles.DetailWeather}></View>
       <StatusBar style='light' />
     </View>
@@ -49,11 +103,9 @@ const styles = StyleSheet.create({
     fontFamily: 'Ubuntu-medium',
     fontSize: 38,
   },
-  weather: {
-    flex: 2,
-  },
+  weather: {},
   day: {
-    flex: 1,
+    width: SCREEN_WIDTH,
     alignItems: 'center',
   },
   today: {
